eb915b7a6a02b3f2ac6f7e980dc7a89efb2eec0f
elmhmmd <youcodemohammed@gmail.com>
2024-09-06 12:08:55 +0100

    bubble sort update

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-int i,tableau[100],array[100],x;
+int i,array[100],swap,x;
 
 printf("combien d'entier voulez vous saisir dans le tableau? ");
 scanf("%d",&i);
@@ -9,22 +9,23 @@ for (int j=0; j<i ; j++)
 {
 printf("Veuillez saisir l'element numero %d: ",j);
 scanf("%d",&array[j]);
-tableau[j]=array[j];
+}
+for(int a=0; a<i ; a++)
+{
+printf("%d ",array[a]);
 }
 for (int y=0;y<i;y++)
 {
-for (int k=0; k<i; k++)
+swap = 0;
+for (int k=0; k<i-1; k++)
 {
 if (array[k]>array[k+1])
 {
  x = array [k+1];
  array[k+1] = array[k];
  array[k] = x;
-}}}
-for(int a=0; a<i ; a++)
-{
-printf("%d ",tableau[a]);
-}
+ swap = 1;
+}}if (swap != 1) {break;}}
 printf("\n");
 for(int a=0; a<i ; a++)
 {
